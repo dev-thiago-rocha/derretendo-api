959538466e85cc88afde8684aaa2e12cee6d3c79
thiago.rocha <thiago.rocha@cwi.com.br>
2021-05-29 00:55:45 -0300

    fix: add cors

--- src/main/java/br/com/thrtec/derretendo/config/SecurityConfig.java
+++ src/main/java/br/com/thrtec/derretendo/config/SecurityConfig.java
@@ -20,8 +20,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
-                .csrf().disable()
+        http
+                .cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/v1/user").permitAll()
                 .anyRequest()
